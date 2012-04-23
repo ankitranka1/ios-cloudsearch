09431c115096b2137685ff082c000c20f91dcfce
ankit ranka <ankitranka@gmail.com>
2012-04-23 00:40:50 -0400

    test commit

---
+++ testGit/testGitTests/testGitTests.h
@@ -0,0 +1,13 @@
+//
+//  testGitTests.h
+//  testGitTests
+//
+//  Created by Ankit Ranka on 4/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testGitTests : SenTestCase
+
+@end
